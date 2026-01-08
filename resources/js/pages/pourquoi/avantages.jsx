5c979fcde27be3edc958160224904db0025e671f
yahya-moussair <yahyamoussair05@gmail.com>
2026-01-08 23:50:15 +0100

    feat : complete mission page v0

--- resources/js/pages/pourquoi/avantages.jsx
+++
@@ -1,7 +0,0 @@
-export default function Avantages() {
-    return (
-        <div>
-            <h1>Avantages</h1>
-        </div>
-    );
-}
--- resources/js/pages/pourquoi/valeurs.jsx
+++
@@ -1,7 +0,0 @@
-export default function Valeurs() {
-    return (
-        <div>
-            <h1>Valeurs</h1>
-        </div>
-    );
-}
