5c0c2950aa53f0d4eb47fc41a1acc351f0330976
forkanimahdi <forkanimahdi@gmail.com>
2026-01-08 21:39:53 +0100

    init: project main color  + addd  logo  +  rename  the  welcome

---
+++ resources/js/pages/home.jsx
@@ -0,0 +1,18 @@
+import AppLayout from '@/layouts/app-layout';
+import AppHeaderLayout from '@/layouts/app/app-header-layout';
+import { dashboard, login, register } from '@/routes';
+
+import { Head, Link, usePage } from '@inertiajs/react';
+
+export default function Home(){
+ 
+    
+
+    return (
+        <>
+     
+     
+     
+        </>
+    );
+}
