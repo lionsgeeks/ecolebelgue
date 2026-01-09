1b30544f1aa7764d4c75c255f75db167b91609ba
Ayman Boujjar <aymenboujjar12@gmail.com>
2026-01-09 02:25:38 +0100

    mergem

--- routes/pourquoi.php
+++ routes/pourquoi.php
@@ -5,15 +5,15 @@ use Inertia\Inertia;
 
 Route::prefix('pourquoi')->name('pourquoi.')->group(function () {
     Route::get('/mission', function () {
-        return Inertia::render('pourquoi/mission');
+        return Inertia::render('pourquoi/mission/index');
     })->name('mission');
 
     Route::get('/valeurs', function () {
-        return Inertia::render('pourquoi/valeurs');
+        return Inertia::render('pourquoi/valeurs/index');
     })->name('valeurs');
 
     Route::get('/avantages', function () {
-        return Inertia::render('pourquoi/avantages');
+        return Inertia::render('pourquoi/avantages/index');
     })->name('avantages');
 });
 
